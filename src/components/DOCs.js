1923c2082950728c05cd3302aae606275a969885
tfius <tadej.fius@gmail.com>
2021-02-12 18:03:55 +0100

    styling, changes to bipselector, upgraded FairLink, FileDropzone extended to support file upload, KVs, PODs, User

--- src/components/DOCs.js
+++ src/components/DOCs.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { FairLink, FairOSApi } from './FairLink'; 
-import {useDropzone} from 'react-dropzone';
+import FileDropzone from './FileDropzone';
 
 function toUTF8Array(str) {
     var utf8 = [];
@@ -33,50 +33,42 @@ function toUTF8Array(str) {
     return utf8;
 }
 
-function MyDropzone(props) {
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
-
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
-        //console.log(reader.result)
-        props.onRead(reader.result);
-      }
-      reader.readAsText(file) //reader.readAsArrayBuffer(file)
-    })
-    
-  }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
-
-  return (
-    <div {...getRootProps()} className="fairDrop">
-      <input {...getInputProps()} />
-      <p>JSON drop or click </p>
-    </div>
-  )
-}
-
 const DOCs = (props) =>  {
     const [apiEndpoint, setApiEndpoint] = useState(props.apiEndpoint); // 
     const [documentTableName, setDocumentTableName] = useState(''); // 
-    const [documentIndexes, setDocumentIndexes] = useState('id=number,timestamp=string,tags=map'); // 
+    const [documentIndexes, setDocumentIndexes] = useState('id=number,name=string,tags=map'); // 
     
     const [documentId, setDocumentId] = useState(''); // 
     const [docKey, setDocKey] = useState("key"+Math.floor(Math.random() * 1000)); //
     const [docValue, setDocValue] = useState(1); // 
-    const [expression, setExpression] = useState('expression'); // 
-    const [start, setStart] = useState(1); // 
-    const [end, setEnd] = useState(5); // 
-    const [noRecords, setNoRecords] = useState(5); // 
+    const [expression, setExpression] = useState(''); // 
+    //const [start, setStart] = useState(1); // 
+    //const [end, setEnd] = useState(5); // 
+    const [noRecords, setNoRecords] = useState(0); // 
+
+    const [url, setUrl] = useState('https://'); // 
 
     const [jsonFile, setJsonFile] = useState('{"jsonSample":"jsonValue"}'); // 
     const [podFile, setPodFile] = useState('{"podSample":"podValue"}'); // 
-    const [jsonDocumentInBytes, setJsonDocumentInBytes] = useState(""); // 
+    const [jsonDocumentInBytes, setJsonDocumentInBytes] = useState('{"id":"1", "name":"John", "surname": "Doe" }'); // 
 
     const [status, setStatus] = useState(null);    // 
     const [error, setError] = useState(null);    //     
+
+    const [statusPut, setStatusPut] = useState(null);    // 
+    const [errorPut, setErrorPut] = useState(null);    //     
+
+    const [statusGet, setStatusGet] = useState(null);    // 
+    const [errorGet, setErrorGet] = useState(null);    //     
+
+    const [statusUrl, setStatusUrl] = useState(null);    // 
+    const [errorUrl, setErrorUrl] = useState(null);    //     
+
+    const [statusIndex, setStatusIndex] = useState(null);    // 
+    const [errorIndex, setErrorIndex] = useState(null);    //     
+
+    const [statusImport, setStatusImport] = useState(null);    // 
+    const [errorImport, setErrorImport] = useState(null);    //     
 
     const [statusExpression, setStatusExpression] = useState(null);    // 
     const [errorExpression, setErrorExpression] = useState(null);    // 
@@ -90,12 +82,12 @@ const DOCs = (props) =>  {
     const docData = new FormData();   
     docData.append("name", documentTableName); 
     docData.append("key", docKey); 
-    docData.append("start",start); 
-    docData.append("end", end); 
+    //docData.append("start",start); 
+    //docData.append("end", end); 
     docData.append("value", docValue); 
     docData.append("limit", noRecords); 
-    docData.append("expr", expression); 
-    docData.append("id", documentId); 
+    docData.append("expr",  expression); 
+    docData.append("id",    documentId); 
     //docData.append("si", documentIndexes);
 
     function docPutData()
@@ -121,23 +113,86 @@ const DOCs = (props) =>  {
     function onStatusExpression(podData)
     {
         console.log(podData);
-        //setStatusExpression(podData);
-        //setErrorExpression(null);
+        setStatusExpression(podData);
+        setErrorExpression(null);
     }
     function onErrorExpression(response)
     {
         console.error("DOCs", response);
         if(response!=undefined)
             setErrorExpression(response.toString());
-
         //setStatusExpression(null);
+    }    
+    function onStatusPut(response)
+    {
+        setStatusPut(response);
+        setErrorPut(null);
     }
+    function onStatusGet(response)
+    {
+        //console.log(response);
+        setStatusGet(atob(response.doc));
+        setErrorGet(null);
+    }
+    function onFindGet(response)
+    {
+        console.log(response.docs);
+        setExpression(atob(response.docs));
+    }
+    function onStatusGetError(response)
+    {
+        console.log(response);
+        if(response!=undefined)
+          setErrorGet(response.toString());
+    }
+    function onStatusIndex(response)
+    {
+        //console.log(response);
+        setStatusIndex(atob(response.doc));
+        setErrorIndex(null);
+    }
+    function onStatusIndexError(response)
+    {
+        console.log(response);
+        if(response!=undefined)
+          setErrorIndex(response.toString());
+        setStatusIndex(null);
+    }
+    function onStatusImport(response)
+    {
+        //console.log(response);
+        setStatusImport(atob(response.doc));
+        setErrorImport(null);
+    }
+    function onStatusImportError(response)
+    {
+        console.log(response);
+        if(response!=undefined)
+          setErrorImport(response.toString());
+        setStatusImport(null);
+    }
+    
     function onError(response)
     {
         console.error("DOCs", response);
         if(response!=undefined)
            setError(response.toString());
         setStatus(null);
+    }
+
+    function checkIfIndexes()
+    {
+        var fData = new FormData();   
+        fData.append("name", documentTableName); 
+        fData.append("key", docKey); 
+        fData.append("value", docValue); 
+        fData.append("limit", noRecords); 
+        fData.append("expr",  expression); 
+        fData.append("id",    documentId); 
+        if(documentIndexes.length>1)
+           fData.append("si", documentIndexes);
+
+        return fData;
     }
     function selectDocument(doc)
     {
@@ -156,12 +211,26 @@ const DOCs = (props) =>  {
         //console.log(docsData);
         setDocuments(docsData["Tables"]);
     }
+    function onCountReceived(countData)
+    {
+        console.log(countData);
+        setNoRecords(countData.message);
+    }
     async function loadSampleData()
     {
         fetch('/sample.data.json').then((r) => r.text()) // included in public folder
         .then(text  => {
           setJsonDocumentInBytes(text);
         }) 
+    }
+
+    function onGetDataFromUrl(e,url)
+    {
+
+    }
+    function onImportData(e,url)
+    {
+
     }
     
     function FairOSApiGetDocuments()
@@ -178,75 +247,103 @@ const DOCs = (props) =>  {
         <div className="leftSide">
                 <h2>DOCs <strong>{documentTableName}</strong></h2>
                     <>
-                        {documents!=null ? documents.map((d,i)=>
-                            <li key={"doc" + i}>
-                            <strong onClick={(e)=>selectDocument(d)} className={d.name==documentTableName ? "selected" : ""}>{d.name}</strong> <span>{d.type}</span>
-                            {d.indexes.map((k,j)=>
-                                    <small key={"index" + k}>
-                                        <small onClick={(e)=>selectIndex(k)}> ({k.name} {k.type})</small><br/>
-                                    </small>
-                                )}  
-                            </li>
-                        ) : null}
+                        <ul>
+                            {documents!=null ? documents.map((d,i)=>
+                                <li key={"doc" + i}>
+                                    <strong onClick={(e)=>selectDocument(d)} className={d.name==documentTableName ? "selected" : ""}>{d.name}</strong>  <span>{d.type}</span>
+
+                                    {d.indexes.length>0 ?
+                                    <>  <br/>
+                                        {d.indexes.map((k,j)=>
+                                                <small key={"index" + j + "_"+d.name} onClick={(e)=>selectIndex(k)}> 
+                                                    <small>({k.name} {k.type}) </small>
+                                                </small>
+                                            )}  
+                                    </> : null }
+                                </li>
+                            ) : null}
+                        </ul>
                     </>    
         </div>
+        
         <div className="rightSide">
                      <input type="text" onChange={(e)=>setDocumentTableName(e.target.value)} value={documentTableName}></input> Document Name <br/>
                      <input type="text" onChange={(e)=>setDocumentIndexes(e.target.value)} value={documentIndexes}></input> Indexes <br/>
                     <div className="fairError">{error}</div>
-                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/new'}  description={"Create New DOC"}    onResult={onStatus}   onError={onError} onAfterGet={FairOSApiGetDocuments}/> 
-                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/open'} description={"Open"}              onResult={onStatus}   onError={onError}/> 
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/new'}  description={"Create New DOC"} onCreateFormData={checkIfIndexes}   onResult={onStatus}   onError={onError} onAfterGet={FairOSApiGetDocuments}/> 
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/open'} description={"Open"}                       onResult={onStatus}   onError={onError}/> 
                     <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/ls'}     description={"List"}     method="get"    onData={onDOCsReceived}  onError={onError}/> 
                     <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/delete'} description={"Delete*"}  method="delete" onResult={onStatus} onError={onError} onAfterGet={FairOSApiGetDocuments}/> 
                     <br/> {status}
                 <hr/> 
                     {/* JSON Data:  <br/> */}
-                    <MyDropzone onRead={setJsonDocumentInBytes}/><br/>
+                    <FileDropzone onRead={setJsonDocumentInBytes}/><br/>
                     <textarea type="textarea" onChange={(e)=>setJsonDocumentInBytes(e.target.value)} value={jsonDocumentInBytes} className="textAreaInput" rows="10"></textarea> <br/>
-                    <FairLink formData={docPutData()}  url={apiEndpoint + '/v0/doc/entry/put'}  description={"Put"} onResult={onStatusExpression}   onError={onErrorExpression} /> 
-                    <button onClick={(e)=>loadSampleData()}>Fetch Sample</button> <br/> 
-                    <br/>{statusExpression}
+
+                    <div className="fairError">{errorPut}</div>
+                    <FairLink formData={docPutData()}  url={apiEndpoint + '/v0/doc/entry/put'}  description={"Put"} onResult={onStatusPut}   onError={setErrorPut} /> 
+                    <button onClick={(e)=>loadSampleData()}>Fetch Sample</button> 
+                    <br/>{statusPut}
                 <hr/>
                     <input type="text" onChange={(e)=>setDocumentId(e.target.value)} value={documentId}></input> DocumentId<br/> 
-                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/entry/get?id'+documentId+'&name='+documentTableName}  description={"Get"}  method="get" onResult={onStatusExpression}   onError={onErrorExpression} /> 
+                    <div className="fairError">{errorGet}</div>
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/entry/get?id='+documentId+'&name='+documentTableName}  description={"Get"}  method="get" onData={onStatusGet} /*onResult={setStatusGet}*/  onError={onStatusGetError} /> 
                     <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/entry/delete'} description={"Delete*"}  method="delete" onResult={onStatus} onError={onError}/> 
-                    <br/>{statusExpression} 
+                    <br/>{statusGet} 
                 <hr/> 
                     
-                    <textarea type="textarea" onChange={(e)=>setJsonFile(e.target.value)} value={jsonFile} className="textAreaInput" rows="5"></textarea> &nbsp; {statusExpression} JSON <br/>
-
-                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/loadjson'} description={"Load"}  onResult={onStatus} onError={onError}/> 
-                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/indexjson'} description={"index"}  onResult={onStatus} onError={onError}/> 
+                    <textarea type="textarea" onChange={(e)=>setJsonFile(e.target.value)} value={jsonFile} className="textAreaInput" rows="5"></textarea> JSON <br/>
+                    <div className="fairError">{errorIndex}</div>            
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/loadjson'} description={"Load"}  onResult={onStatusIndex} onError={onStatusIndexError}/> 
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/indexjson'} description={"index"}  onResult={onStatusIndex} onError={onStatusIndexError}/> 
+                    <br/>{statusIndex} 
+                <hr/> 
+                    <input type="text" onChange={(e)=>setExpression(e.target.value)} value={expression} ></input> Expression (name=John)
+                    <div className="fairError">{errorExpression}</div>
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/count'} description={"Count"} onData={onCountReceived} onResult={onStatusExpression}   onError={onErrorExpression} /> 
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/find?name='+documentTableName+'&expr='+expression}  description={"Find"} method="get" 
+                              onData={onFindGet}  onResult={onStatusExpression}   onError={onErrorExpression} /> 
+                    <input type="text" onChange={(e)=>setNoRecords(e.target.value)} value={noRecords} style={{maxWidth:"5%",minWidth:"10%"}}></input> Limit
+                    <br/> {statusExpression}                     
 
                 <hr/> 
-                    <input type="text" onChange={(e)=>setExpression(e.target.value)} value={expression} ></input> Expression
-                    <div className="fairError">{errorExpression}</div>
-                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/count'} description={"Count"} onResult={onStatusExpression}   onError={onErrorExpression} /> 
-                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/find'}  description={"Find"}  onResult={onStatusExpression}   onError={onErrorExpression} /> 
-                    <br/> {statusExpression}                     
+                    <input type="text" onChange={(e)=>setUrl(e.target.value)} value={url} ></input> Import From URL 
+                    <div className="fairError">{errorImport}</div>
+
+                    <button className="fairLink" onClick={(e)=>onGetDataFromUrl(e,url)}>{"GET"}</button>
+                    <button className="fairLink" onClick={(e)=>onImportData(e,url)}>{"Import"}</button>
+{/* 
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/count'} description={"GET"}     onResult={onStatusImport} onError={onStatusImportError} /> 
+                    <FairLink formData={docData}  url={apiEndpoint + '/v0/doc/find'}  description={"Import"}  onResult={onStatusImport} onError={onStatusImportError} />  */}
+                    <br/> {statusImport}                     
+   
         </div>
     </div>
         <hr/>
-        <ul>
-              <strong>DOCs  APIs</strong>
-              <li>POST -F 'name=\{documentTableName}' {apiEndpoint}/v0/doc/new</li>
-              <li>POST -F 'name=\{documentTableName}' {apiEndpoint}/v0/doc/open</li>
-              
-              <li>POST {apiEndpoint}/v0/doc/ls</li>
-              <li>DELETE -F 'name=\{documentTableName}' {apiEndpoint}/v0/doc/delete</li>
-        </ul>
-        <ul>  <strong>DOCs Search APIs</strong>
-              <li>POST -F 'name=\{documentTableName}' -F 'expr=\{expression}' {apiEndpoint}/v0/doc/count</li>
-              <li>POST -F 'name=\{documentTableName}' -F 'expr=\{expression}' -F 'limit=\{noRecords}' {apiEndpoint}/v0/doc/find</li>
-        </ul>
-              
-        <ul>  <strong>DOCs Entry APIs</strong>
-              <li>POST -F 'name=\{documentTableName}' -F 'doc=\{jsonDocumentInBytes}' {apiEndpoint}/v0/doc/entry/put</li>
-              <li>GET -F 'name=\{documentTableName}' -F 'id=\{documentId}' {apiEndpoint}/v0/doc/entry/get</li>
-              <li>DELETE -F 'name=\{documentTableName}' -F 'id=\{documentId}' {apiEndpoint}/v0/doc/entry/del</li>
-              <li>POST -F 'name=\{documentTableName}' -F 'json=@\{jsonFile}' {apiEndpoint}/v0/doc/loadjson</li>
-              <li>POST -F 'name=\{documentTableName}' -F 'file=\{podFile}' {apiEndpoint}/v0/doc/indexjson</li>
-        </ul>
+        {window.showDebug==true ? 
+        <>            
+            <ul>
+                <strong>DOCs  APIs</strong>
+                <li>POST -F 'name=\{documentTableName}' {apiEndpoint}/v0/doc/new</li>
+                <li>POST -F 'name=\{documentTableName}' {apiEndpoint}/v0/doc/open</li>
+                
+                <li>POST {apiEndpoint}/v0/doc/ls</li>
+                <li>DELETE -F 'name=\{documentTableName}' {apiEndpoint}/v0/doc/delete</li>
+            </ul>
+            <ul>  <strong>DOCs Search APIs</strong>
+                <li>POST -F 'name=\{documentTableName}' -F 'expr=\{expression}' {apiEndpoint}/v0/doc/count</li>
+                <li>POST -F 'name=\{documentTableName}' -F 'expr=\{expression}' -F 'limit=\{noRecords}' {apiEndpoint}/v0/doc/find</li>
+            </ul>
+                
+            <ul>  <strong>DOCs Entry APIs</strong>
+                <li>POST -F 'name=\{documentTableName}' -F 'doc=\{jsonDocumentInBytes}' {apiEndpoint}/v0/doc/entry/put</li>
+                <li>GET -F 'name=\{documentTableName}' -F 'id=\{documentId}' {apiEndpoint}/v0/doc/entry/get</li>
+                <li>DELETE -F 'name=\{documentTableName}' -F 'id=\{documentId}' {apiEndpoint}/v0/doc/entry/del</li>
+                <li>POST -F 'name=\{documentTableName}' -F 'json=@\{jsonFile}' {apiEndpoint}/v0/doc/loadjson</li>
+                <li>POST -F 'name=\{documentTableName}' -F 'file=\{podFile}' {apiEndpoint}/v0/doc/indexjson</li>
+            </ul>
+        </> 
+        : null }
     </>
       );
   }
